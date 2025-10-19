e700e768844ecc64c6553aa6cb09085572ae38b0
Streaming2024 <workforyubraj@gmail.com>
2025-10-19 11:34:29 +0545

    Update love.js

--- love.js
+++ love.js
@@ -26,7 +26,7 @@ const streamMap = {
   },
   "IND2": {
     url: "https://live-en.aisports.cc/moviebox/device02/playlist.m3u8",
-    poster: "https://imgeng.jagran.com/images/2025/09/30/article/image/complete-list-of-ICC-women-cricket-world-cup-winners--1759237177116.webp"
+    poster: "https://nirutv.online/logo/image.png"
   },
   "WOMENS": {
     url: "https://hugh.cdn.rumble.cloud/live/t5qc33px/slot-131/k3xj-agbx/chunklist.m3u8",

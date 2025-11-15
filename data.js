9f58857b89ef8ba007c8f0d9517727e895badc4a
Streaming2024 <workforyubraj@gmail.com>
2025-11-15 22:41:38 +0545

    Create data.js

---
+++ data.js
@@ -0,0 +1,24 @@
+window.matchCardData = {
+  "teams": [
+    { "name": "Man United", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/10260.png" },
+    { "name": "Real Madrid", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8633.png" },
+    { "name": "Man City", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8456.png" },
+    { "name": "Al Nassr", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/101918.png" },
+    { "name": "Barcelona", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8634.png" },
+    { "name": "Bayern Munich", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/9277.png" },
+    { "name": "PSG", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8586.png" },
+    { "name": "Liverpool", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8650.png" },
+    { "name": "Chelsea", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/8455.png" },
+    { "name": "Arsenal", "logo": "https://images.fotmob.com/image_resources/logo/teamlogo/9825.png" }
+    // Add more teams as needed
+  ],
+  "leagues": [
+    "T20I Series", "ODI Series", "Test Series", "Nepal Premier League", "T20 world cup",
+    "ODI world cup", "Women's Asia Cup", "Women's World Cup", "Asia cup", "Champions Trophy",
+    "FIFA Club World Cup", "Ligue 1", "Premier League", "ICC CWC League 2",
+    "Bundesliga", "Serie A", "La Liga", "UEFA Champions League", "UEFA Europa League",
+    "African Nations Championship", "AFC Champions League", "Botola Pro", "Serie A Women",
+    "Pro League", "CAF Confederation Cup", "Botola 2"
+    // Add more leagues as needed
+  ]
+};
